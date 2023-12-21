7428a068e3179c58840fd22a7591a3eb0144eaab
Buferxon <buferxon@gmail.com>
2023-12-21 14:49:31 -0500

    Correcion

--- qditxnextjs/src/app/layout.jsx
+++ qditxnextjs/src/app/layout.jsx
@@ -1,5 +1,4 @@
-import { Inter } from "next/font/google"; // Corrige la importación
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap"; // Asegúrate de importar los componentes de react-bootstrap
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,35 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
-				<Navbar expand="lg" className="bg-body-tertiary">
-					<Container>
-						<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-						<Navbar.Toggle aria-controls="basic-navbar-nav" />
-						<Navbar.Collapse id="basic-navbar-nav">
-							<Nav className="me-auto">
-								<Nav.Link href="#home">Home</Nav.Link>
-								<Nav.Link href="#link">Link</Nav.Link>
-								<NavDropdown title="Dropdown" id="basic-nav-dropdown">
-									<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-									<NavDropdown.Item href="#action/3.2">
-										Another action
-									</NavDropdown.Item>
-									<NavDropdown.Item href="#action/3.3">
-										Something
-									</NavDropdown.Item>
-									<NavDropdown.Divider />
-									<NavDropdown.Item href="#action/3.4">
-										Separated link
-									</NavDropdown.Item>
-								</NavDropdown>
-							</Nav>
-						</Navbar.Collapse>
-					</Container>
-				</Navbar>
-
-				{children}
-			</body>
+			<body className={inter.className}>{children}</body>
 		</html>
 	);
 }
