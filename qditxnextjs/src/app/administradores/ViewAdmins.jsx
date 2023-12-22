2f9687eaf3efc7e519054bf27dd82e28f3ccf690
Buferxon <buferxon@gmail.com>
2023-12-22 11:38:12 -0500

    Mejora del bucador

--- qditxnextjs/src/app/administradores/ViewAdmins.jsx
+++ qditxnextjs/src/app/administradores/ViewAdmins.jsx
@@ -2,7 +2,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import { BiSearch, BiRefresh } from "react-bootstrap-icons";
-import Skeleton from "react-loading-skeleton";
 
 function ViewAdmins() {
 	const [users, setUsers] = useState([]);
@@ -16,7 +15,6 @@ function ViewAdmins() {
 	const [pageNumber, setPageNumber] = useState(0);
 	const isFirstRender = useRef(true);
 	const [currentFilters, setCurrentFilters] = useState({});
-	const [isLoading, setIsLoading] = useState(true);
 
 	const getAdmins = async (page, filters) => {
 		try {
@@ -52,14 +50,12 @@ function ViewAdmins() {
 					url += `&estado=${estado}`;
 				}
 			}
-
 			console.log("URL " + url);
 			const response = await fetch(url);
 			const jsonData = await response.json();
 
 			setUsers(jsonData);
 			setResults(jsonData);
-			setIsLoading(false);
 		} catch (err) {
 			console.error(err.message);
 		}
@@ -73,11 +69,10 @@ function ViewAdmins() {
 		}
 
 		getAdmins(pageNumber);
-		setIsLoading(true);
 	}, [pageNumber]);
 
 	function buscar() {
-		getAdmins(pageNumber, { cc, nuser, names, last_names, numero, estado });
+		getAdmins(0, { cc, nuser, names, last_names, numero, estado });
 	}
 
 	function limpiar() {
@@ -99,6 +94,7 @@ function ViewAdmins() {
 		setNumero("");
 		setEstado("");
 		setResults(users);
+		setPageNumber(0);
 	}
 
 	function paginacion(page) {
@@ -110,267 +106,124 @@ function ViewAdmins() {
 		getAdmins(pageNumber);
 	}
 
-	const loader = () => {
-		return (
-			<>
-				<h1 className="text-dark center text-center">
-					Lista de Administradores
-				</h1>
-				<div className="row">
-					<div className="col-md-2">
-						<input
-							value={cc}
-							onChange={(e) => {
-								setCc(e.target.value);
-							}}
-							type="number"
-							placeholder="Por Identificacion"
-							className="form-control"
-						/>
-					</div>
-					<div className="col-md-2">
-						<input
-							value={nuser}
-							onChange={(e) => {
-								setNuser(e.target.value);
-							}}
-							type="text"
-							placeholder="Por Nombre de Usuario"
-							className="form-control"
-						/>
-					</div>
-					<div className="col-md-2">
-						<input
-							value={names}
-							onChange={(e) => {
-								setNames(e.target.value);
-							}}
-							type="text"
-							placeholder="Por Nombre"
-							className="form-control"
-						/>
-					</div>
-					<div className="col-md-2">
-						<input
-							value={last_names}
-							onChange={(e) => {
-								setLast_names(e.target.value);
-							}}
-							type="text"
-							placeholder="Por Apellidos"
-							className="form-control"
-						/>
-					</div>
-					<div className="col-md-2">
-						<input
-							value={numero}
-							onChange={(e) => {
-								setNumero(e.target.value);
-							}}
-							type="text"
-							placeholder="Por Numero Telefonico"
-							className="form-control"
-						/>
-					</div>
-					<div className="col-md-2">
-						<select
-							value={estado}
-							onChange={(e) => {
-								setEstado(e.target.value);
-							}}
-							className="form-control"
-						>
-							<option value="">Filtro por Estado</option>
-							<option value="activo">Activo</option>
-							<option value="inactivo">Inactivo</option>
-						</select>
-					</div>
-					<div className="row mt-2">
-						<div className="col-md-6">
-							<button className="btn btn-primary mr-2" onClick={buscar}>
-								Buscar
-							</button>
-							<button className="btn btn-secondary" onClick={limpiar}>
-								Limpiar
-							</button>
-						</div>
+	return (
+		<>
+			<h1 className="text-dark center text-center">Lista de Administradores</h1>
+			<div className="row">
+				<div className="col-md-2">
+					<input
+						value={cc}
+						onChange={(e) => {
+							setCc(e.target.value);
+						}}
+						type="number"
+						placeholder="Por Identificacion"
+						className="form-control"
+					/>
+				</div>
+				<div className="col-md-2">
+					<input
+						value={nuser}
+						onChange={(e) => {
+							setNuser(e.target.value);
+						}}
+						type="text"
+						placeholder="Por Nombre de Usuario"
+						className="form-control"
+					/>
+				</div>
+				<div className="col-md-2">
+					<input
+						value={names}
+						onChange={(e) => {
+							setNames(e.target.value);
+						}}
+						type="text"
+						placeholder="Por Nombre"
+						className="form-control"
+					/>
+				</div>
+				<div className="col-md-2">
+					<input
+						value={last_names}
+						onChange={(e) => {
+							setLast_names(e.target.value);
+						}}
+						type="text"
+						placeholder="Por Apellidos"
+						className="form-control"
+					/>
+				</div>
+				<div className="col-md-2">
+					<input
+						value={numero}
+						onChange={(e) => {
+							setNumero(e.target.value);
+						}}
+						type="text"
+						placeholder="Por Numero Telefonico"
+						className="form-control"
+					/>
+				</div>
+				<div className="col-md-2">
+					<select
+						value={estado}
+						onChange={(e) => {
+							setEstado(e.target.value);
+						}}
+						className="form-control"
+					>
+						<option value="">Filtro por Estado</option>
+						<option value="activo">Activo</option>
+						<option value="inactivo">Inactivo</option>
+					</select>
+				</div>
+				<div className="row mt-2">
+					<div className="col-md-6">
+						<button className="btn btn-primary mr-2" onClick={buscar}>
+							Buscar
+						</button>
+						<button className="btn btn-secondary" onClick={limpiar}>
+							Limpiar
+						</button>
 					</div>
 				</div>
-
-				<table className="table table-striped table-bordered table-hover mt-5 text-center">
-					<thead className="thead-dark">
-						<tr>
-							<th>Nombres</th>
-							<th>Apellidos</th>
-							<th>Identificacion</th>
-							<th>Usuario</th>
-							<th>Roles</th>
-							<th>Movil</th>
-							<th>Estado</th>
-						</tr>
-					</thead>
-					<tbody>
-						{Array.isArray(results) &&
-							results.map((user, index) => (
-								<tr
-									key={user.id}
-									className={index % 2 === 0 ? "table-light" : ""} // Aplica clase para filas pares
-								>
-									<td>
-										<Skeleton />
-									</td>
-									<td>
-										<Skeleton />
-									</td>
-									<td>
-										<Skeleton />
-									</td>
-									<td>
-										<Skeleton />
-									</td>
-									<td>
-										<Skeleton />
-									</td>
-									<td>
-										<Skeleton />
-									</td>
-									<td>
-										<Skeleton />
-									</td>
-								</tr>
-							))}
-					</tbody>
-				</table>
-			</>
-		);
-	};
-
-	if (isLoading) {
-		return loader();
-	} else {
-		return (
-			<>
-				<h1 className="text-dark center text-center">
-					Lista de Administradores
-				</h1>
-				<div className="row">
-					<div className="col-md-2">
-						<input
-							value={cc}
-							onChange={(e) => {
-								setCc(e.target.value);
-							}}
-							type="number"
-							placeholder="Por Identificacion"
-							className="form-control"
-						/>
-					</div>
-					<div className="col-md-2">
-						<input
-							value={nuser}
-							onChange={(e) => {
-								setNuser(e.target.value);
-							}}
-							type="text"
-							placeholder="Por Nombre de Usuario"
-							className="form-control"
-						/>
-					</div>
-					<div className="col-md-2">
-						<input
-							value={names}
-							onChange={(e) => {
-								setNames(e.target.value);
-							}}
-							type="text"
-							placeholder="Por Nombre"
-							className="form-control"
-						/>
-					</div>
-					<div className="col-md-2">
-						<input
-							value={last_names}
-							onChange={(e) => {
-								setLast_names(e.target.value);
-							}}
-							type="text"
-							placeholder="Por Apellidos"
-							className="form-control"
-						/>
-					</div>
-					<div className="col-md-2">
-						<input
-							value={numero}
-							onChange={(e) => {
-								setNumero(e.target.value);
-							}}
-							type="text"
-							placeholder="Por Numero Telefonico"
-							className="form-control"
-						/>
-					</div>
-					<div className="col-md-2">
-						<select
-							value={estado}
-							onChange={(e) => {
-								setEstado(e.target.value);
-							}}
-							className="form-control"
-						>
-							<option value="">Filtro por Estado</option>
-							<option value="activo">Activo</option>
-							<option value="inactivo">Inactivo</option>
-						</select>
-					</div>
-					<div className="row mt-2">
-						<div className="col-md-6">
-							<button className="btn btn-primary mr-2" onClick={buscar}>
-								Buscar
-							</button>
-							<button className="btn btn-secondary" onClick={limpiar}>
-								Limpiar
-							</button>
-						</div>
-					</div>
-				</div>
-
-				<table className="table table-striped table-bordered table-hover mt-5 text-center">
-					<thead className="thead-dark">
-						<tr>
-							<th>Nombres</th>
-							<th>Apellidos</th>
-							<th>Identificacion</th>
-							<th>Usuario</th>
-							<th>Roles</th>
-							<th>Movil</th>
-							<th>Estado</th>
-						</tr>
-					</thead>
-					<tbody>
-						{Array.isArray(results) &&
-							results.map((user, index) => (
-								<tr
-									key={user.id}
-									className={index % 2 === 0 ? "table-light" : ""} // Aplica clase para filas pares
-								>
-									<td>{user.name}</td>
-									<td>{user.last_name}</td>
-									<td>{user.identification}</td>
-									<td>{user.user_name}</td>
-									<td>{user.roles}</td>
-									<td>{user.movil}</td>
-									<td>{user.status === "ACTIVE" ? "Activo" : "Inactivo"}</td>
-								</tr>
-							))}
-					</tbody>
-				</table>
-
-				<button onClick={() => paginacion(0)}>Anterior</button>
-				<span>Página {pageNumber + 1}</span>
-				<button onClick={() => paginacion(1)}>Siguiente</button>
-			</>
-		);
-	}
+			</div>
+			<table className="table table-striped table-bordered table-hover mt-5 text-center">
+				<thead className="thead-dark">
+					<tr>
+						<th>Nombres</th>
+						<th>Apellidos</th>
+						<th>Identificacion</th>
+						<th>Usuario</th>
+						<th>Roles</th>
+						<th>Movil</th>
+						<th>Estado</th>
+					</tr>
+				</thead>
+				<tbody>
+					{Array.isArray(results) &&
+						results.map((user, index) => (
+							<tr
+								key={user.id}
+								className={index % 2 === 0 ? "table-light" : ""} // Aplica clase para filas pares
+							>
+								<td>{user.name}</td>
+								<td>{user.last_name}</td>
+								<td>{user.identification}</td>
+								<td>{user.user_name}</td>
+								<td>{user.roles}</td>
+								<td>{user.movil}</td>
+								<td>{user.status === "ACTIVE" ? "Activo" : "Inactivo"}</td>
+							</tr>
+						))}
+				</tbody>
+			</table>
+			<button onClick={() => paginacion(0)}>Anterior</button>
+			<span>Página {pageNumber + 1}</span>
+			<button onClick={() => paginacion(1)}>Siguiente</button>
+		</>
+	);
 }
 
 export default ViewAdmins;
